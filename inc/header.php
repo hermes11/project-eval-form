e732b4ef7b9ff05d85e5a8ea068b45f00901e124
hermes11 <54098574+hermes11@users.noreply.github.com>
2019-08-28 16:00:21 +0800

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Account</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<style>
+	.btn{
+		float:right;
+		position:relative;
+		
+	}
+.hori{
+	float: left;
+  	width: 33.33%;
+ 	 padding: 5px;
+}
+.row{
+	display: flex;
+}
+h2{
+	color:purple;
+}
+
+</style>
+<body>
+	<?php include('navbar.php'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ inc/navbar.php
@@ -0,0 +1,24 @@
+<nav class=" navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+
+  <a class="navbar-brand" href="#">User Account</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li>
+        <a class="nav-link" href="home.php">Home</a>
+      </li>
+    
+    </ul>
+     <a style="color:white;" type="text" class="nav-link" href="eval.php">Evaluation Page</a>
+     <a style="color:white;" type="text" class="nav-link" href="welcome.php">Log-out</a>
+  </div>
+</nav>
+
+
+
+
+
